a913af65b81e19315c3bd78525e22760d329a4ff
mlab1 <mlabrecque@gmail.com>
2023-06-06 07:40:50 -0400

    init

---
+++ bin/my_app2.dart
@@ -0,0 +1,15 @@
+import 'dart:math';
+
+void main() {
+  print("testing123");
+
+  var isTrue = true;
+
+  if (isTrue)
+    print("is true");
+  else
+    print("is false");
+
+  var number = Random().nextInt(100);
+  print("$number is a good number");
+}
